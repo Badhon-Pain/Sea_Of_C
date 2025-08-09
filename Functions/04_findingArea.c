a15e24188ba500faf356750a1a65015eeea8bace
Test_Case <badhonpain48@gmail.com>
2025-08-09 20:59:34 +0600

    handling invalid commands

--- Functions/04_findingArea.c
+++ Functions/04_findingArea.c
@@ -7,6 +7,7 @@ void print_Welcome_message()
 {
     printf("Welcome to the Area Calculator!\n");
     printf("You can calculate the area of different shapes.\n");
+    printf("If you want to stop calculating just type 'None' instead of any shape.\n");
     printf("Available shapes:\n");
     printf("1. Rectangle\n");
     printf("2. Circle\n");
@@ -40,31 +41,42 @@ int main()
     print_Welcome_message();
     char shape[20];
     float length, width, radius, side, base, height;
-    printf("Choose a shape to calculate it's area: ");
-    scanf("%s", shape);
-    if (strcmp(shape, "Rectangle") == 0)
+    while (1)
     {
-        printf("Enter length & width : ");
-        scanf("%f %f", &length, &width);
-        area_Rectangle(length, width);
-    }
-    else if (strcmp(shape, "Circle") == 0)
-    {
-        printf("Enter radius : ");
-        scanf("%f", &radius);
-        area_Circle(radius);
-    }
-    else if (strcmp(shape, "Triangle") == 0)
-    {
-        printf("Enter base & height : ");
-        scanf("%f %f", &base, &height);
-        area_Triangle(base, height);
-    }
-    else if (strcmp(shape, "Square") == 0)
-    {
-        printf("Enter side : ");
-        scanf("%f", &side);
-        area_Square(side);
+        printf("Choose a shape to calculate it's area: ");
+        scanf("%s", shape);
+        if (strcmp(shape, "Rectangle") == 0)
+        {
+            printf("Enter length & width : ");
+            scanf("%f %f", &length, &width);
+            area_Rectangle(length, width);
+        }
+        else if (strcmp(shape, "Circle") == 0)
+        {
+            printf("Enter radius : ");
+            scanf("%f", &radius);
+            area_Circle(radius);
+        }
+        else if (strcmp(shape, "Triangle") == 0)
+        {
+            printf("Enter base & height : ");
+            scanf("%f %f", &base, &height);
+            area_Triangle(base, height);
+        }
+        else if (strcmp(shape, "Square") == 0)
+        {
+            printf("Enter side : ");
+            scanf("%f", &side);
+            area_Square(side);
+        }
+        else if (strcmp(shape, "None") == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("Invalid Shape. Enter a regular shape\n");
+        }
     }
 
     return 0;
